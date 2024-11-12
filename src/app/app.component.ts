c3f42e4438d24f0a91248f02a7a4e11325cc8d14
moisgames <VLAD981010@gmail.com>
2024-11-12 16:46:43 +0700

    refactor(common-ui) Добавил Компонент list-chart и добавил его в app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListChartComponent } from "./common-ui/list-chart/list-chart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/common-ui/list-chart/list-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-chart.component.html',
+  styleUrl: './list-chart.component.scss'
+})
+export class ListChartComponent {
+
+}
